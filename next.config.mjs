e00804263fb646d97570d49df0318727200c1d3c
iamharikrishnaa <iamharikrishnaa@gmail.com>
2025-09-10 01:02:53 +0530

    force

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-const isCI = process.env.GITHUB_ACTIONS === 'true';
-
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
@@ -9,12 +7,8 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-    unoptimized: true, // required for GitHub Pages
+    unoptimized: true,
   },
-  output: 'export',     // 👈 ensures Next.js generates ./out for static hosting
-  // Optional: manually set paths if you don’t want configure-pages to inject them
-  // basePath: isCI ? '/<your-repo-name>' : '',
-  // assetPrefix: isCI ? '/<your-repo-name>/' : '',
-};
+}
 
-export default nextConfig;
+export default nextConfig
